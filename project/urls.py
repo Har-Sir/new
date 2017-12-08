bc63775eb8a3aac0d487daf365d1554183a1d7b5
Guomingyin <1226768228@qq.com>
2017-12-08 20:14:50 +0800

    更新代码

--- project/urls.py
+++ project/urls.py
@@ -15,14 +15,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
 
-from django.conf.urls import url
+from django.conf.urls import url, include
 from django.contrib import admin
-from front import user as user_views
+from front import views as user_views
 from master import manager as manager_view
+
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-
     #前台
     #前台注册
     url(r'^u_register', user_views.u_register,name='u_register'),
@@ -34,11 +34,16 @@ urlpatterns = [
     url(r'^u_main',user_views.u_main,name='u_main'),
     #在线订票
     url(r'^u_booking', user_views.u_booking,name='u_booking'),
+    #购票
+    url(r'^u_buy', user_views.u_buy,name='u_buy'),
     #帮助中心
     url(r'^u_help',user_views.u_help,name='u_help'),
     #个人中心
     url(r'^u_own',user_views.u_own,name='u_own'),
     url(r'^u_suggestion',user_views.u_suggestion,name='u_suggestion'),
+
+    # # users:info
+    # url(r'^user/', include('front.urls', namespace="users")),
 
     #后台
     #管理员登录
@@ -57,11 +62,9 @@ urlpatterns = [
     url(r'^m_order', manager_view.m_order,name='m_order'),
     #车讯管理
     url(r'^m_inform', manager_view.m_inform,name='m_inform'),
-    #公告管理
-    url(r'^m_announce', manager_view.m_announce,name='m_announce'),
+    #帮助信息
+    url(r'^m_help', manager_view.m_help,name='m_help'),
     #意见反馈
     url(r'^m_suggest', manager_view.m_suggest,name='m_suggest'),
-
-
 
 ]
